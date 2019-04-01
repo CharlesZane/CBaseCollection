c2dda7cda2d76c67470542de0513f1c84343324f
CharlesZane <zhancx1138@126.com>
2019-04-01 14:35:59 +0800

    1、工程创建

---
+++ CBaseCollection/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  CBaseCollection
+//
+//  Created by sola on 2019/4/1.
+//  Copyright © 2019 sola. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    int mai = 2;
+    
+    return 0;
+}
